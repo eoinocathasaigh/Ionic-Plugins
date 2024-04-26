69c812a96e80ec312eaf1dbfeb6bc57407fae6a0
unknown <G00417466@atu.ie>
2024-04-26 11:48:53 +0000

    Plugin Lab Complete - Browser & Geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Motion } from '@capacitor/motion';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
   constructor() {}
+  coordinates:any = "";
+  lat:number = 0;
+  long:number = 0;
+  time:any = 0;
+
+  //Method to handle getting our gps location
+  async getGPS(){
+    this.coordinates = await Geolocation.getCurrentPosition();
+    this.long = this.coordinates.coords.longitude;
+    this.lat = this.coordinates.coords.latitude;
+    this.time = new Date(this.coordinates.timestamp);
+  }
+
+  //Method to handle our browser plugin
+  async openBrowser(){
+    //This line will open the browser in a new tab for us once the button is clicked
+    await Browser.open({ url: 'https://vlegalwaymayo.atu.ie/' });
+  }
 }
